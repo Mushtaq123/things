d2151efc945d72ed7349891bf17556e0d4a82e1c
Alexandru Simonescu <alexandru.simonescu@gmail.com>
2016-12-17 01:57:12 +0100

    Dummy code to try debugging

--- app/src/main/java/com/alexsimo/things/MainActivity.java
+++ app/src/main/java/com/alexsimo/things/MainActivity.java
@@ -1,12 +1,16 @@
 package com.alexsimo.things;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+  private static final String TAG = MainActivity.class.getSimpleName();
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Log.d(TAG, "Hello Android Things!");
   }
 }
